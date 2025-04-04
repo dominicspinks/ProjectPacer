d7dae2c847ef2ab2c934dfe5dc7e38e02fabff6f
Dominic Spinks <dominic.spinks@outlook.com>
2025-04-05 09:57:13 +1100

    Fix issue with add task modal

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -64,7 +64,7 @@ export default function UserDefaultTaskList({
                 )}
             </table>
 
-            <NewTaskModal visible={showModal} handleCloseModal={cleanModal} handleSaveTask={handleSaveTask} currentTask={currentTask} setCurrentTask={setCurrentTask} />
+            <NewTaskModal visible={showModal} handleCloseModal={cleanModal} handleSaveTask={handleSaveTask} currentTask={currentTask} setCurrentTask={setCurrentTask} setShowModal={setShowModal} />
         </div>
     );
 }
